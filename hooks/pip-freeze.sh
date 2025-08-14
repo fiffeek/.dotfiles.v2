c495144fff1e763c1485e0f3931f2c820cf5ed9b
Filip Mikina <filipmikina@gmail.com>
2025-08-14 11:59:46 +0200

    pip freeze to /tmp first, otherwise the reqs file has mtime changed
    and so `make install` has to rerun; this fixes the behavior

--- hooks/pip-freeze.sh
+++ hooks/pip-freeze.sh
@@ -4,6 +4,15 @@ DIR="$(dirname "$(realpath "$0")")"
 
 source "$DIR/../venv/bin/activate"
 
+TMP_FILE="$(mktemp)"
+REQUIREMENTS_FILE="requirements.txt"
+
 pip freeze | sed 's/_/-/g' |
   awk -F '==' '{ printf "%s==%s\n", tolower($1), $2 }' \
-    >requirements.txt
+    >"$TMP_FILE"
+
+if ! cmp -s "$TMP_FILE" "$REQUIREMENTS_FILE"; then
+  mv "$TMP_FILE" "$REQUIREMENTS_FILE"
+else
+  rm "$TMP_FILE"
+fi
