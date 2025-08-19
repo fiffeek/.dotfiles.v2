d9ba05cd0d2c942a5c0f536cb99266ca0d0a7a62
Filip Mikina <filipmikina@gmail.com>
2025-08-19 14:08:32 +0200

    add the missing summary to the refresh rate notification

--- ansible/files/framework/bin/hypr/adjust-refresh-rate.sh
+++ ansible/files/framework/bin/hypr/adjust-refresh-rate.sh
@@ -10,11 +10,11 @@ if [ "$POWER_STATUS" -eq 1 ]; then
     exit 0
   fi
   hyprctl keyword monitor "eDP-1,2880x1920@120.00000,0x0,2.0,vrr,1"
-  notify-send "Refresh rate 120" || true
+  notify-send "Monitor adjusted" "Refresh rate 120" || true
 else
   if [[ "$HZ" =~ 60.* ]]; then
     exit 0
   fi
   hyprctl keyword monitor "eDP-1,2880x1920@60.00000,0x0,2.0,vrr,1"
-  notify-send "Refresh rate 60" || true
+  notify-send "Monitor adjusted" "Refresh rate 60" || true
 fi
