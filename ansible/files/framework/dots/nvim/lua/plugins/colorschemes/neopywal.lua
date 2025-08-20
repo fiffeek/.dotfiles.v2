c29a2850def1691a3ff7b10422203a2402ca5ff3
Filip Mikina <filipmikina@gmail.com>
2025-08-20 14:43:36 +0200

    add plugins to neopywal for lualine and misc
    https://github.com/RedsXDD/neopywal.nvim

--- ansible/files/framework/dots/nvim/lua/plugins/colorschemes/neopywal.lua
+++ ansible/files/framework/dots/nvim/lua/plugins/colorschemes/neopywal.lua
@@ -1,12 +1,54 @@
 return {
-  "RedsXDD/neopywal.nvim",
-  name = "neopywal",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("neopywal").setup({
-      use_wallust = false,
-      transparent_background = true,
-    })
-  end,
+  {
+    "RedsXDD/neopywal.nvim",
+    name = "neopywal",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("neopywal").setup({
+        use_wallust = false,
+        transparent_background = true,
+        plugins = {
+          fzf = true,
+          harpoon = true,
+          leap = {
+            enabled = true,
+            style = { "bold", "italic" },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    config = function()
+      require("bufferline").setup({
+        highlights = require("neopywal.theme.plugins.bufferline").setup(),
+      })
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    enabled = true,
+    config = function()
+      local has_lualine, lualine = pcall(require, "lualine")
+      if not has_lualine then
+        return
+      end
+
+      local has_neopywal, neopywal_lualine = pcall(require, "neopywal.theme.plugins.lualine")
+      if not has_neopywal then
+        return
+      end
+
+      neopywal_lualine.setup()
+
+      lualine.setup({
+        options = {
+          theme = "neopywal",
+        },
+      })
+    end,
+  },
 }
