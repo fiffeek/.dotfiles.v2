ecd1ae485ef3dccad6c71f6aefce9177b6a48cde
Filip Mikina <filipmikina@gmail.com>
2025-08-25 19:01:54 +0200

    add nvim keybinds for window switching and resizing with arrows
    colemak ugh

--- ansible/files/framework/dots/nvim/lua/config/keymaps.lua
+++ ansible/files/framework/dots/nvim/lua/config/keymaps.lua
@@ -12,3 +12,15 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- move to window using the <ctrl> arrow keys
+vim.keymap.set("n", "<C-Up>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+vim.keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+vim.keymap.set("n", "<C-Left>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+vim.keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<leader><C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.keymap.set("n", "<leader><C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+vim.keymap.set("n", "<leader><C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+vim.keymap.set("n", "<leader><C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
