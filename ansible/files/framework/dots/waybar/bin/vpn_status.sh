d3890cea55077059801a09afb4b543d2349896c7
Filip Mikina <filipmikina@gmail.com>
2025-09-03 20:22:41 +0200

    fix: do not exit on grep error, mullvad nukes nmcli conn entirely

--- ansible/files/framework/dots/waybar/bin/vpn_status.sh
+++ ansible/files/framework/dots/waybar/bin/vpn_status.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 WIREGUARD_CONN_NAME=${1:-homelab}
 VPN_GET_STATUS=$(nmcli -f STATE,TYPE,NAME conn show | grep "$WIREGUARD_CONN_NAME")
 VPN_STATUS=$(echo "$VPN_GET_STATUS" | awk '/wireguard/ { print $1 }')
